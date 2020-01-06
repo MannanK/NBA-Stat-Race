028ad45a886b4ea217dca19a7c113c3422129022
Mannan Kasliwal <mkasliw1@binghamton.edu>
2020-01-06 15:32:14 -0500

    Make HTML for top row with season dropdown, stat dropdown, and glossary button; add some CSS for overall main page and top row

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ const testObj = {
   }
 };
 
-const greeting = testObj?.key2?.key3 || testObj.key1;
-window.addEventListener("DOMContentLoaded", () => {
-  document.body.classList.add("center");
-  const card = document.createElement("div");
-  card.classList.add("card", "center");
-  card.innerHTML = `<h2>${greeting} World!</h2>`;
-  document.body.append(card);
-});
+// const greeting = testObj?.key2?.key3 || testObj.key1;
+// window.addEventListener("DOMContentLoaded", () => {
+//   document.body.classList.add("center");
+//   const card = document.createElement("div");
+//   card.classList.add("card", "center");
+//   card.innerHTML = `<h2>${greeting} World!</h2>`;
+//   document.body.append(card);
+// });
