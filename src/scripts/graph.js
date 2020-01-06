a1ca2ea633417110472d55f13973d0123a042dc9
Mannan Kasliwal <mkasliw1@binghamton.edu>
2020-01-06 18:41:08 -0500

    Start making the svg element, margin convention

--- src/scripts/graph.js
+++ src/scripts/graph.js
@@ -1,10 +1,16 @@
 function makeGraph() {
-  let margin = { top: 50, right: 50, bottom: 50, left: 50 };
+  let margin = { top: 40, right: 40, bottom: 40, left: 40 };
   let docGraphWidth = document.getElementById("graph-container").clientWidth;
   let docGraphHeight = document.getElementById("graph-container").clientHeight;
 
   let width = docGraphWidth - margin.left - margin.right;
   let height = docGraphHeight - margin.top - margin.bottom;
+
+  let svg = d3.select('#graph-container').append("svg")
+      .attr("width", docGraphWidth)
+      .attr("height", docGraphHeight)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 }
 
 module.exports = {
