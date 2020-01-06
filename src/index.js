e4593867b8ce888c95281e4c194222bcf8991ec3
Mannan Kasliwal <mkasliw1@binghamton.edu>
2020-01-06 13:26:00 -0500

    Set up boilerplate code, get webpack working

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./styles/index.scss";
+const testObj = {
+  key1: "hi",
+  key2: {
+    key3: "Hello"
+  }
+};
+
+const greeting = testObj?.key2?.key3 || testObj.key1;
+window.addEventListener("DOMContentLoaded", () => {
+  document.body.classList.add("center");
+  const card = document.createElement("div");
+  card.classList.add("card", "center");
+  card.innerHTML = `<h2>${greeting} World!</h2>`;
+  document.body.append(card);
+});
---
+++ webpack.common.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const outputDir = "./dist";
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"), //
+  output: {
+    path: path.join(__dirname, outputDir),
+    filename: "[name].js",
+    publicPath: "/dist/"
+  },
+  resolve: {
+    extensions: [".js"] // if we were using React.js, we would include ".jsx"
+  },
+  module: {
+    rules: [{
+      test: /\.js$/, // if we were using React.js, we would use \.jsx?$/
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+          plugins: ["@babel/plugin-proposal-optional-chaining"],
+          exclude: /node_modules/
+        } // if we were using React.js, we would include "react"
+      }
+    },
+    {
+      test: /\.css$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          // you can specify a publicPath here
+          // by default it uses publicPath in webpackOptions.output
+          publicPath: "../",
+          hmr: process.env.NODE_ENV === "development"
+        }
+      },
+        "css-loader",
+        "postcss-loader"
+      ]
+    },
+    {
+      test: /\.scss/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          // you can specify a publicPath here
+          // by default it uses publicPath in webpackOptions.output
+          publicPath: "../",
+          hmr: process.env.NODE_ENV === "development"
+        }
+      },
+        "css-loader",
+        "sass-loader",
+        "postcss-loader"
+      ]
+    }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // all options are optional
+    filename: "[name].css",
+    chunkFilename: "[id].css",
+    ignoreOrder: false // Enable to remove warnings about conflicting order
+  }), require("autoprefixer")]
+};
