d4fcb327c4a41e961ace239b708bf48a8a18062a
Mannan Kasliwal <mkasliw1@binghamton.edu>
2020-01-06 21:19:14 -0500

    Add hardcoded data to use for now

--- src/assets/mockData.js
+++ src/assets/mockData.js
@@ -1,6 +1,4 @@
-// hardcoded data for now
-// game # : total stat so far
-let data = {
+let threePointersMade = {
   stephenCurry: {
     '1': 3,
     '2': 7,
@@ -202,28 +200,4 @@ let data = {
     '69': 85,
     '70': 85
   }
-};
-
-function makeGraph() {
-  let margin = { top: 40, right: 40, bottom: 40, left: 40 };
-  let docGraphWidth = document.getElementById("graph-container").clientWidth;
-  let docGraphHeight = document.getElementById("graph-container").clientHeight;
-
-  let width = docGraphWidth - margin.left - margin.right;
-  let height = docGraphHeight - margin.top - margin.bottom;
-
-  // Margin convention and make the graph resize as the window resizes
-  // From now on, all subsequent code can just use 'width' and 'height'
-  let svg = d3.select('#graph-container').append("svg")
-      .attr("width", "100%")
-      .attr("height", "100%")
-      .attr('viewBox', '0 0 ' + Math.min(docGraphWidth, docGraphHeight) + ' ' + Math.min(docGraphWidth, docGraphHeight))
-      .attr('preserveAspectRatio', 'xMinYMin');
-
-  let g = svg.append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-}
-
-module.exports = {
-  makeGraph
 };
