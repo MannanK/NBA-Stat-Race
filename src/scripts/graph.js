255db6d8f21f94da4ec32b39b216cef46a83632d
Mannan Kasliwal <mkasliw1@binghamton.edu>
2020-01-08 14:58:57 -0500

    Reformated graph.js to work with data where data comes in as an array, each element is an object with keys 'name' and 'values', where 'name' is the player name and 'values' is an array of objects with each object representing a game and total

--- src/scripts/graph.js
+++ src/scripts/graph.js
@@ -1,211 +1,220 @@
 // hardcoded data for now
 // game # : total stat so far
-let data = {
-  stephenCurry: [
-    { game: 0, total: 0 },
-    { game: 1, total: 3 },
-    { game: 2, total: 7 },
-    { game: 3, total: 13 },
-    { game: 4, total: 15 },
-    { game: 5, total: 19 },
-    { game: 6, total: 22 },
-    { game: 7, total: 25 },
-    { game: 8, total: 32 },
-    { game: 9, total: 35 },
-    { game: 10, total: 40 },
-    { game: 11, total: 42 },
-    { game: 12, total: 44 },
-    { game: 13, total: 47 },
-    { game: 14, total: 49 },
-    { game: 15, total: 53 },
-    { game: 16, total: 57 },
-    { game: 17, total: 60 },
-    { game: 18, total: 64 },
-    { game: 19, total: 67 },
-    { game: 20, total: 70 },
-    { game: 21, total: 73 },
-    { game: 22, total: 78 },
-    { game: 23, total: 83 },
-    { game: 24, total: 93 },
-    { game: 25, total: 99 },
-    { game: 26, total: 103 },
-    { game: 27, total: 111 },
-    { game: 28, total: 116 },
-    { game: 29, total: 118 },
-    { game: 30, total: 122 },
-    { game: 31, total: 128 },
-    { game: 32, total: 133 },
-    { game: 33, total: 141 },
-    { game: 34, total: 146 },
-    { game: 35, total: 154 },
-    { game: 36, total: 155 },
-    { game: 37, total: 158 },
-    { game: 38, total: 163 },
-    { game: 39, total: 165 },
-    { game: 40, total: 169 },
-    { game: 41, total: 172 },
-    { game: 42, total: 175 },
-    { game: 43, total: 178 },
-    { game: 44, total: 186 },
-    { game: 45, total: 191 },
-    { game: 46, total: 195 },
-    { game: 47, total: 199 },
-    { game: 48, total: 203 },
-    { game: 49, total: 209 },
-    { game: 50, total: 209 },
-    { game: 51, total: 212 }
-  ],
-  joelEmbiid: [
-    { game: 0, total: 0 },
-    { game: 1, total: 0 },
-    { game: 2, total: 0 },
-    { game: 3, total: 1 },
-    { game: 4, total: 2 },
-    { game: 5, total: 5 },
-    { game: 6, total: 5 },
-    { game: 7, total: 6 },
-    { game: 8, total: 6 },
-    { game: 9, total: 7 },
-    { game: 10, total: 7 },
-    { game: 11, total: 8 },
-    { game: 12, total: 10 },
-    { game: 13, total: 11 },
-    { game: 14, total: 11 },
-    { game: 15, total: 12 },
-    { game: 16, total: 13 },
-    { game: 17, total: 13 },
-    { game: 18, total: 13 },
-    { game: 19, total: 13 },
-    { game: 20, total: 13 },
-    { game: 21, total: 15 },
-    { game: 22, total: 15 },
-    { game: 23, total: 16 },
-    { game: 24, total: 16 },
-    { game: 25, total: 17 },
-    { game: 26, total: 19 },
-    { game: 27, total: 25 },
-    { game: 28, total: 25 },
-    { game: 29, total: 25 },
-    { game: 30, total: 25 },
-    { game: 31, total: 26 },
-    { game: 32, total: 27 },
-    { game: 33, total: 28 },
-    { game: 34, total: 28 },
-    { game: 35, total: 30 },
-    { game: 36, total: 30 },
-    { game: 37, total: 31 },
-    { game: 38, total: 32 },
-    { game: 39, total: 35 },
-    { game: 40, total: 36 },
-    { game: 41, total: 36 },
-    { game: 42, total: 36 },
-    { game: 43, total: 39 },
-    { game: 44, total: 41 },
-    { game: 45, total: 42 },
-    { game: 46, total: 43 },
-    { game: 47, total: 43 },
-    { game: 48, total: 46 },
-    { game: 49, total: 47 },
-    { game: 50, total: 49 },
-    { game: 51, total: 52 },
-    { game: 52, total: 52 },
-    { game: 53, total: 53 },
-    { game: 54, total: 53 },
-    { game: 55, total: 53 },
-    { game: 56, total: 56 },
-    { game: 57, total: 57 },
-    { game: 58, total: 57 },
-    { game: 59, total: 59 },
-    { game: 60, total: 60 },
-    { game: 61, total: 64 },
-    { game: 62, total: 64 },
-    { game: 63, total: 65 },
-    { game: 64, total: 66 },
-    { game: 65, total: 66 },
-    { game: 66, total: 66 },
-    { game: 67, total: 69 },
-    { game: 68, total: 69 },
-    { game: 69, total: 69 },
-    { game: 70, total: 71 },
-    { game: 71, total: 72 },
-    { game: 72, total: 72 },
-    { game: 73, total: 74 },
-    { game: 74, total: 74 }
-  ],
-  georgeHill: [
-    { game: 0, total: 0 },
-    { game: 1, total: 1 },
-    { game: 2, total: 4 },
-    { game: 3, total: 4 },
-    { game: 4, total: 4 },
-    { game: 5, total: 4 },
-    { game: 6, total: 4 },
-    { game: 7, total: 6 },
-    { game: 8, total: 6 },
-    { game: 9, total: 6 },
-    { game: 10, total: 7 },
-    { game: 11, total: 10 },
-    { game: 12, total: 11 },
-    { game: 13, total: 12 },
-    { game: 14, total: 14 },
-    { game: 15, total: 17 },
-    { game: 16, total: 17 },
-    { game: 17, total: 18 },
-    { game: 18, total: 22 },
-    { game: 19, total: 22 },
-    { game: 20, total: 22 },
-    { game: 21, total: 22 },
-    { game: 22, total: 24 },
-    { game: 23, total: 25 },
-    { game: 24, total: 27 },
-    { game: 25, total: 31 },
-    { game: 26, total: 35 },
-    { game: 27, total: 39 },
-    { game: 28, total: 40 },
-    { game: 29, total: 40 },
-    { game: 30, total: 40 },
-    { game: 31, total: 41 },
-    { game: 32, total: 42 },
-    { game: 33, total: 45 },
-    { game: 34, total: 46 },
-    { game: 35, total: 46 },
-    { game: 36, total: 48 },
-    { game: 37, total: 52 },
-    { game: 38, total: 52 },
-    { game: 39, total: 52 },
-    { game: 40, total: 52 },
-    { game: 41, total: 52 },
-    { game: 42, total: 55 },
-    { game: 43, total: 56 },
-    { game: 44, total: 56 },
-    { game: 45, total: 56 },
-    { game: 46, total: 58 },
-    { game: 47, total: 60 },
-    { game: 48, total: 61 },
-    { game: 49, total: 61 },
-    { game: 50, total: 62 },
-    { game: 51, total: 62 },
-    { game: 52, total: 65 },
-    { game: 53, total: 65 },
-    { game: 54, total: 68 },
-    { game: 55, total: 68 },
-    { game: 56, total: 69 },
-    { game: 57, total: 71 },
-    { game: 58, total: 72 },
-    { game: 59, total: 74 },
-    { game: 60, total: 76 },
-    { game: 61, total: 77 },
-    { game: 62, total: 77 },
-    { game: 63, total: 79 },
-    { game: 64, total: 80 },
-    { game: 65, total: 83 },
-    { game: 66, total: 83 },
-    { game: 67, total: 84 },
-    { game: 68, total: 84 },
-    { game: 69, total: 85 },
-    { game: 70, total: 85 }
-  ]
-};
+let data = [
+  {
+    "name": "Stephen Curry",
+    "values": [
+      { game: 0, total: 0 },
+      { game: 1, total: 3 },
+      { game: 2, total: 7 },
+      { game: 3, total: 13 },
+      { game: 4, total: 15 },
+      { game: 5, total: 19 },
+      { game: 6, total: 22 },
+      { game: 7, total: 25 },
+      { game: 8, total: 32 },
+      { game: 9, total: 35 },
+      { game: 10, total: 40 },
+      { game: 11, total: 42 },
+      { game: 12, total: 44 },
+      { game: 13, total: 47 },
+      { game: 14, total: 49 },
+      { game: 15, total: 53 },
+      { game: 16, total: 57 },
+      { game: 17, total: 60 },
+      { game: 18, total: 64 },
+      { game: 19, total: 67 },
+      { game: 20, total: 70 },
+      { game: 21, total: 73 },
+      { game: 22, total: 78 },
+      { game: 23, total: 83 },
+      { game: 24, total: 93 },
+      { game: 25, total: 99 },
+      { game: 26, total: 103 },
+      { game: 27, total: 111 },
+      { game: 28, total: 116 },
+      { game: 29, total: 118 },
+      { game: 30, total: 122 },
+      { game: 31, total: 128 },
+      { game: 32, total: 133 },
+      { game: 33, total: 141 },
+      { game: 34, total: 146 },
+      { game: 35, total: 154 },
+      { game: 36, total: 155 },
+      { game: 37, total: 158 },
+      { game: 38, total: 163 },
+      { game: 39, total: 165 },
+      { game: 40, total: 169 },
+      { game: 41, total: 172 },
+      { game: 42, total: 175 },
+      { game: 43, total: 178 },
+      { game: 44, total: 186 },
+      { game: 45, total: 191 },
+      { game: 46, total: 195 },
+      { game: 47, total: 199 },
+      { game: 48, total: 203 },
+      { game: 49, total: 209 },
+      { game: 50, total: 209 },
+      { game: 51, total: 212 }
+    ]
+  },
+  {
+    "name": "Joel Embiid",
+    "values": [
+      { game: 0, total: 0 },
+      { game: 1, total: 0 },
+      { game: 2, total: 0 },
+      { game: 3, total: 1 },
+      { game: 4, total: 2 },
+      { game: 5, total: 5 },
+      { game: 6, total: 5 },
+      { game: 7, total: 6 },
+      { game: 8, total: 6 },
+      { game: 9, total: 7 },
+      { game: 10, total: 7 },
+      { game: 11, total: 8 },
+      { game: 12, total: 10 },
+      { game: 13, total: 11 },
+      { game: 14, total: 11 },
+      { game: 15, total: 12 },
+      { game: 16, total: 13 },
+      { game: 17, total: 13 },
+      { game: 18, total: 13 },
+      { game: 19, total: 13 },
+      { game: 20, total: 13 },
+      { game: 21, total: 15 },
+      { game: 22, total: 15 },
+      { game: 23, total: 16 },
+      { game: 24, total: 16 },
+      { game: 25, total: 17 },
+      { game: 26, total: 19 },
+      { game: 27, total: 25 },
+      { game: 28, total: 25 },
+      { game: 29, total: 25 },
+      { game: 30, total: 25 },
+      { game: 31, total: 26 },
+      { game: 32, total: 27 },
+      { game: 33, total: 28 },
+      { game: 34, total: 28 },
+      { game: 35, total: 30 },
+      { game: 36, total: 30 },
+      { game: 37, total: 31 },
+      { game: 38, total: 32 },
+      { game: 39, total: 35 },
+      { game: 40, total: 36 },
+      { game: 41, total: 36 },
+      { game: 42, total: 36 },
+      { game: 43, total: 39 },
+      { game: 44, total: 41 },
+      { game: 45, total: 42 },
+      { game: 46, total: 43 },
+      { game: 47, total: 43 },
+      { game: 48, total: 46 },
+      { game: 49, total: 47 },
+      { game: 50, total: 49 },
+      { game: 51, total: 52 },
+      { game: 52, total: 52 },
+      { game: 53, total: 53 },
+      { game: 54, total: 53 },
+      { game: 55, total: 53 },
+      { game: 56, total: 56 },
+      { game: 57, total: 57 },
+      { game: 58, total: 57 },
+      { game: 59, total: 59 },
+      { game: 60, total: 60 },
+      { game: 61, total: 64 },
+      { game: 62, total: 64 },
+      { game: 63, total: 65 },
+      { game: 64, total: 66 },
+      { game: 65, total: 66 },
+      { game: 66, total: 66 },
+      { game: 67, total: 69 },
+      { game: 68, total: 69 },
+      { game: 69, total: 69 },
+      { game: 70, total: 71 },
+      { game: 71, total: 72 },
+      { game: 72, total: 72 },
+      { game: 73, total: 74 },
+      { game: 74, total: 74 }
+    ]
+  },
+  {
+    "name": "George Hill",
+    "values": [
+      { game: 0, total: 0 },
+      { game: 1, total: 1 },
+      { game: 2, total: 4 },
+      { game: 3, total: 4 },
+      { game: 4, total: 4 },
+      { game: 5, total: 4 },
+      { game: 6, total: 4 },
+      { game: 7, total: 6 },
+      { game: 8, total: 6 },
+      { game: 9, total: 6 },
+      { game: 10, total: 7 },
+      { game: 11, total: 10 },
+      { game: 12, total: 11 },
+      { game: 13, total: 12 },
+      { game: 14, total: 14 },
+      { game: 15, total: 17 },
+      { game: 16, total: 17 },
+      { game: 17, total: 18 },
+      { game: 18, total: 22 },
+      { game: 19, total: 22 },
+      { game: 20, total: 22 },
+      { game: 21, total: 22 },
+      { game: 22, total: 24 },
+      { game: 23, total: 25 },
+      { game: 24, total: 27 },
+      { game: 25, total: 31 },
+      { game: 26, total: 35 },
+      { game: 27, total: 39 },
+      { game: 28, total: 40 },
+      { game: 29, total: 40 },
+      { game: 30, total: 40 },
+      { game: 31, total: 41 },
+      { game: 32, total: 42 },
+      { game: 33, total: 45 },
+      { game: 34, total: 46 },
+      { game: 35, total: 46 },
+      { game: 36, total: 48 },
+      { game: 37, total: 52 },
+      { game: 38, total: 52 },
+      { game: 39, total: 52 },
+      { game: 40, total: 52 },
+      { game: 41, total: 52 },
+      { game: 42, total: 55 },
+      { game: 43, total: 56 },
+      { game: 44, total: 56 },
+      { game: 45, total: 56 },
+      { game: 46, total: 58 },
+      { game: 47, total: 60 },
+      { game: 48, total: 61 },
+      { game: 49, total: 61 },
+      { game: 50, total: 62 },
+      { game: 51, total: 62 },
+      { game: 52, total: 65 },
+      { game: 53, total: 65 },
+      { game: 54, total: 68 },
+      { game: 55, total: 68 },
+      { game: 56, total: 69 },
+      { game: 57, total: 71 },
+      { game: 58, total: 72 },
+      { game: 59, total: 74 },
+      { game: 60, total: 76 },
+      { game: 61, total: 77 },
+      { game: 62, total: 77 },
+      { game: 63, total: 79 },
+      { game: 64, total: 80 },
+      { game: 65, total: 83 },
+      { game: 66, total: 83 },
+      { game: 67, total: 84 },
+      { game: 68, total: 84 },
+      { game: 69, total: 85 },
+      { game: 70, total: 85 }
+    ]
+  }
+];
 
 function resizingFunction(svg) {
   let width = parseInt(svg.style('width'), 10);
@@ -248,8 +257,10 @@ export function makeGraph() {
   let g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  let maxTotal = Math.max(...Object.values(data).map(player => {
-    return player[player.length - 1].total;
+  let maxTotal = Math.max(...data.map(obj => {
+    return Math.max(...Object.values(obj.values).map(player => {
+      return player.total;
+    }));
   }));
 
   let xScale = d3.scaleLinear()
@@ -286,59 +297,110 @@ export function makeGraph() {
     .attr("dy", "0.8em")
     .text("Total");
 
+  // // for each object (player) in the data object, make the lines
+  // Object.values(data).forEach((player, i) => {
+  //   // make the group element for the line
+  //   linesContainer.selectAll(`.line-group-${i + 1}`)
+  //     .data([player])
+  //     .enter()
+  //     .append("g")
+  //     .attr("class", `line-group line-group-${i + 1}`)
+  //     // put the actual line on the screen
+  //     .append("path")
+  //     .datum(player)
+  //     .attr("class", "line")
+  //     .attr("fill", "none")
+  //     .attr("stroke", color(i + 1))
+  //     .attr("d", line);
+  // });
+
   // for each object (player) in the data object, make the lines
-  Object.values(data).forEach((player, i) => {
-    // make the group element for the line
-    linesContainer.selectAll(`.line-group-${i + 1}`)
-      .data([player])
-      .enter()
-      .append("g")
-      .attr("class", `line-group line-group-${i + 1}`)
-      // put the actual line on the screen
-      .append("path")
-      .datum(player)
-      .attr("class", "line")
-      .attr("fill", "none")
-      .attr("stroke", color(i + 1))
-      .attr("d", line);
-  });
+  // make the group element for the line
+  linesContainer.selectAll(`.line-group`)
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("class", `line-group`)
+    // put the actual line on the screen
+    .append("path")
+    .attr('d', d => line(d.values))
+    .attr("class", "line")
+    .attr("fill", "none")
+    .attr("stroke", (d, i) => color(i))
+
+  // // for each object (player) in the data object, make the dots and text
+  // Object.values(data).forEach((player, i) => {
+  //   // make the group element for the dots
+  //   linesContainer.selectAll(`.dot-group-${i + 1}`)
+  //     .data([player])
+  //     .enter()
+  //     .append("g")
+  //     .attr("class", `dot-group dot-group-${i + 1}`)
+  //     .selectAll(`.dot-container-${i + 1}`)
+  //     .data(player)
+  //     .enter()
+  //     // make the group element container for each dot one by one (because of data)
+  //     .append("g")
+  //     .attr("class", `dot-container dot-container-${i + 1}`)
+  //     .on("mouseover", function (d) {
+  //       d3.select(this)
+  //         .style("cursor", "pointer")
+  //         .append("text")
+  //         .attr("class", "text")
+  //         .text(`Game: ${d.game}, Total: ${d.total}`)
+  //         .attr('text-anchor', 'middle')
+  //         .attr("x", d => xScale(d.game))
+  //         .attr("y", d => yScale(d.total) - 15);
+  //     })
+  //     .on("mouseout", function (d) {
+  //       d3.select(this)
+  //         .style("cursor", "none")
+  //         .selectAll(".text").remove();
+  //     })
+  //     // make the actual dot
+  //     .append("circle")
+  //     .attr("class", `dot dot-${i + 1}`)
+  //     .attr("stroke", color(i + 1))
+  //     .attr("fill", "white")
+  //     .attr("cx", function (d) { return xScale(d.game); })
+  //     .attr("cy", function (d) { return yScale(d.total); })
+  //     .attr("r", ".25%");
+  // });
 
   // for each object (player) in the data object, make the dots and text
-  Object.values(data).forEach((player, i) => {
-    // make the group element for the dots
-    linesContainer.selectAll(`.dot-group-${i + 1}`)
-      .data([player])
-      .enter()
-      .append("g")
-      .attr("class", `dot-group dot-group-${i + 1}`)
-      .selectAll(`.dot-container-${i + 1}`)
-      .data(player)
-      .enter()
-      // make the group element container for each dot one by one (because of data)
-      .append("g")
-      .attr("class", `dot-container dot-container-${i + 1}`)
-      .on("mouseover", function (d) {
-        d3.select(this)
-          .style("cursor", "pointer")
-          .append("text")
-          .attr("class", "text")
-          .text(`Game: ${d.game}, Total: ${d.total}`)
-          .attr('text-anchor', 'middle')
-          .attr("x", d => xScale(d.game))
-          .attr("y", d => yScale(d.total) - 15);
-      })
-      .on("mouseout", function (d) {
-        d3.select(this)
-          .style("cursor", "none")
-          .selectAll(".text").remove();
-      })
-      // make the actual dot
-      .append("circle")
-      .attr("class", `dot dot-${i + 1}`)
-      .attr("stroke", color(i + 1))
-      .attr("fill", "white")
-      .attr("cx", function (d) { return xScale(d.game); })
-      .attr("cy", function (d) { return yScale(d.total); })
-      .attr("r", ".25%");
-  });
+  // make the group element for the dots
+  linesContainer.selectAll(`.dot-group`)
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("class", `dot-group`)
+    .selectAll(`.dot-container`)
+    .data(d => d.values)
+    .enter()
+    // make the group element container for each dot one by one (because of data)
+    .append("g")
+    .attr("class", `dot-container`)
+    .on("mouseover", function (d) {
+      d3.select(this)
+        .style("cursor", "pointer")
+        .append("text")
+        .attr("class", "text")
+        .text(`Game: ${d.game}, Total: ${d.total}`)
+        .attr('text-anchor', 'middle')
+        .attr("x", d => xScale(d.game))
+        .attr("y", d => yScale(d.total) - 15);
+    })
+    .on("mouseout", function (d) {
+      d3.select(this)
+        .style("cursor", "none")
+        .selectAll(".text").remove();
+    })
+    // make the actual dot
+    .append("circle")
+    .attr("class", `dot`)
+    .attr("stroke", (d, i) => color(i))
+    .attr("fill", "white")
+    .attr("cx", d => xScale(d.game))
+    .attr("cy", d => yScale(d.total))
+    .attr("r", ".25%");
 }
